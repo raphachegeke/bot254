150b5423192158348ee2deb84e1930dcb4d54473
raphachegeke <raphachegekamunu@gmail.com>
2026-04-05 10:11:50 +0300

    added port

--- index.js
+++ index.js
@@ -322,3 +322,17 @@ fs.watchFile(file, () => {
     delete require.cache[file]
     require(file)
 })
+
+
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Bot is running 🚀");
+});
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
