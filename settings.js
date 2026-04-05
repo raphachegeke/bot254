e60c8df8465d78520a5eec60fe37401868baed42
Rapha Chege Kamunu <raphachegekamunu@gmail.com>
2026-04-05 09:14:55 +0300

    Update bot settings for name and owner

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const settings = {
-  packname: 'Knight Bot',
-  author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  packname: 'Rapha',
+  author: 'Rapha‎',
+  botName: "Rapha Bot",
+  botOwner: 'Leon', // Your name
+  ownerNumber: '0748397839', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "2.0.3",
 };
