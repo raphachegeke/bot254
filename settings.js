da450adae9311a72055be3156f6d1625a7ded112
Rapha Chege Kamunu <raphachegekamunu@gmail.com>
2026-05-25 20:19:39 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'Rapha‎',
   botName: "Rapha Bot",
   botOwner: 'Leon', // Your name
-  ownerNumber: '0748397839', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '0788010778', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
